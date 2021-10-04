740f7c7d19e04e4f727689e77faafa75d192f015
fathularif <fathulcode@gmail.com>
2021-10-04 18:29:59 +0800

    first commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+
+<body>
+    <article>
+        <h1><a href="/posts/my-first-post">My first post</a></h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor consectetur fuga nam vitae expedita
+            consequatur facere officia id tempora ut dignissimos, natus iure placeat. Officia voluptatem ad in illum
+            nemo.</p>
+    </article>
+    <article>
+        <h1><a href="/posts/my-second-post">My secone post</a></h1>
+
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor consectetur fuga nam vitae expedita
+            consequatur facere officia id tempora ut dignissimos, natus iure placeat. Officia voluptatem ad in illum
+            nemo.</p>
+    </article>
+    <article>
+        <h1><a href="/posts/my-third-post">My thirs post</a></h1>
+
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor consectetur fuga nam vitae expedita
+            consequatur facere officia id tempora ut dignissimos, natus iure placeat. Officia voluptatem ad in illum
+            nemo.</p>
+    </article>
+    <article>
+        <h1><a href="/posts/my-forth-post">My fort post</a></h1>
+
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor consectetur fuga nam vitae expedita
+            consequatur facere officia id tempora ut dignissimos, natus iure placeat. Officia voluptatem ad in illum
+            nemo.</p>
+    </article>
+</body>
+
+</html>
