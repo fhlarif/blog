d21f3ffd49ca181806a78539b1a4b66769cdaf9a
fathularif <fathulcode@gmail.com>
2021-10-05 17:23:57 +0800

    add cache to post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -26,7 +26,8 @@ class Post
 
     public static function all()
     {
-        return collect(File::files(resource_path("posts")))
+        return cache()->rememberForever('post.all',function(){
+            return collect(File::files(resource_path("posts")))
             ->map(fn ($file) => YamlFrontMatter::parseFile($file))
             ->map(fn ($document) => new Post(
                 $document->title,
@@ -34,7 +35,9 @@ class Post
                 $document->date,
                 $document->body(),
                 $document->slug
-            ));
+            ))->sortByDesc('date');
+        });
+
     }
 
     public static function find($slug)
