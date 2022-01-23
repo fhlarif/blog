65a64324b4679d36436771d771d085eccfc5e7db
fathularif <fathulcode@gmail.com>
2022-01-23 10:14:05 +0800

    view all post by author

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,53 +17,60 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-
+        /**if not use fresh, then use truncate */
         User::truncate();
         Category::truncate();
         Post::truncate();
 
-        $user = User::factory()->create();
+        /**can use this to overide post factory user created */
+        // $user = User::factory()->create();
+        // Post::factory(5)->create([
+        //     'user_id' => $user->id,
+        // ]);
 
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
-        ]);
+        Post::factory(5)->create();
 
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family'
-        ]);
 
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
+        // $personal = Category::create([
+        //     'name' => 'Personal',
+        //     'slug' => 'personal'
+        // ]);
 
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $personal->id,
-            'title' => 'My Personal Post',
-            'slug' => 'my-personal-post',
-            'excerpt' => 'Lorem ipsum dolor, sit amet',
-            'body' => '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus, voluptas laborum molestias ea facilis quaerat dignissimos repellat voluptatibus odit ipsam quasi vitae vel ipsa possimus dicta tempora reprehenderit a explicabo!</p>',
-        ]);
+        // $family = Category::create([
+        //     'name' => 'Family',
+        //     'slug' => 'family'
+        // ]);
 
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My Work Post',
-            'slug' => 'my-work-post',
-            'excerpt' => 'Lorem ipsum dolor, sit amet',
-            'body' => '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus, voluptas laborum molestias ea facilis quaerat dignissimos repellat voluptatibus odit ipsam quasi vitae vel ipsa possimus dicta tempora reprehenderit a explicabo!</p>',
-        ]);
+        // $work = Category::create([
+        //     'name' => 'Work',
+        //     'slug' => 'work'
+        // ]);
 
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $family->id,
-            'title' => 'My Family Post',
-            'slug' => 'my-family-post',
-            'excerpt' => 'Lorem ipsum dolor, sit amet',
-            'body' => '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus, voluptas laborum molestias ea facilis quaerat dignissimos repellat voluptatibus odit ipsam quasi vitae vel ipsa possimus dicta tempora reprehenderit a explicabo!</p>',
-        ]);
+        // Post::create([
+        //     'user_id' => $user->id,
+        //     'category_id' => $personal->id,
+        //     'title' => 'My Personal Post',
+        //     'slug' => 'my-personal-post',
+        //     'excerpt' => 'Lorem ipsum dolor, sit amet',
+        //     'body' => '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus, voluptas laborum molestias ea facilis quaerat dignissimos repellat voluptatibus odit ipsam quasi vitae vel ipsa possimus dicta tempora reprehenderit a explicabo!</p>',
+        // ]);
+
+        // Post::create([
+        //     'user_id' => $user->id,
+        //     'category_id' => $work->id,
+        //     'title' => 'My Work Post',
+        //     'slug' => 'my-work-post',
+        //     'excerpt' => 'Lorem ipsum dolor, sit amet',
+        //     'body' => '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus, voluptas laborum molestias ea facilis quaerat dignissimos repellat voluptatibus odit ipsam quasi vitae vel ipsa possimus dicta tempora reprehenderit a explicabo!</p>',
+        // ]);
+
+        // Post::create([
+        //     'user_id' => $user->id,
+        //     'category_id' => $family->id,
+        //     'title' => 'My Family Post',
+        //     'slug' => 'my-family-post',
+        //     'excerpt' => 'Lorem ipsum dolor, sit amet',
+        //     'body' => '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus, voluptas laborum molestias ea facilis quaerat dignissimos repellat voluptatibus odit ipsam quasi vitae vel ipsa possimus dicta tempora reprehenderit a explicabo!</p>',
+        // ]);
     }
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,15 +1,16 @@
 <x-app>
     @foreach ($posts as $post)
-        <article>
-            <a href="/posts/{{ $post->slug}}">
-                <h1>{{ $post->title }}</h1>
-            </a>
-            <p>
-                <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
-            </p>
-            <div>
-                <p>{{ $post->excerpt }}</p>
-            </div>
-        </article>
+    <article>
+        <a href="/posts/{{ $post->slug}}">
+            <h1>{{ $post->title }}</h1>
+        </a>
+        <p>
+            By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a
+                href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+        </p>
+        <div>
+            <p>{{ $post->excerpt }}</p>
+        </div>
+    </article>
     @endforeach
 </x-app>
